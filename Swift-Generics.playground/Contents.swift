060f3d991db9f3f1f6cf30a71cc21dcb1baf5804
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-05 11:57:08 -0700

    insert and remove

--- Swift-Generics.playground/Contents.swift
+++ Swift-Generics.playground/Contents.swift
@@ -11,21 +11,59 @@ myCountedSet.remove(.dwarvish) // 0
 myCountedSet.remove(.magic) // 0
 */
 
+
+var dict = ["a": 1, "b": 2, "c": 3]
+dict["c"] = 4
+dict["d"] = 1
+
+
+dict
+
 struct CountedSet<E: Hashable> {
-	private (set) var elements: [E]?
+	private (set) var contents: [E: Int] = [:]
 	
-	subscript(_ element: E) -> Int {
-		guard let elements = elements else { return 0 }
-		return elements.count
+	//get set
+	subscript(_ member: E) -> Int {
+		
+		return contents[member, default: 0]
 	}
+	
+	mutating func insert(_ member: E) {
+		contents[member, default: 0] += 1
+	}
+	
+	mutating func remove(_ member: E) {
+		if let value = contents[member] {
+			if value > 1 {
+				contents[member, default: 0] -= 1
+			} else {
+				contents.removeValue(forKey: member)
+			}
+			
+		}
+	}
+
 }
+
+extension CountedSet: ExpressibleByArrayLiteral {
+	
+	
+	
+}
+
 
 enum Arrow { case iron, elven, dwarvish, magic, silver }
 
 
-var aCountedSet = CountedSet<Arrow>(elements: [.dwarvish, .elven, .iron])
-aCountedSet[.iron]
+var aCountedSet = CountedSet<Arrow>()
+//aCountedSet[.iron]
+aCountedSet.insert(.elven)
+aCountedSet[.elven]
+aCountedSet.remove(.elven)
+aCountedSet
 
+
+//var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
 
 //var myCountedSet: CountedSet<Arrow> = [.iron]
 //myCountedSet[.iron] // 4
