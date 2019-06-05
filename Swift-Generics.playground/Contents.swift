d8acad2902ec470e92a49f3232a625ecb30bb4af
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-04 23:07:20 -0700

    subscript

--- Swift-Generics.playground/Contents.swift
+++ Swift-Generics.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /*
 enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
-var aCountedSet = CountedSet<Arrow>()
+var aCountedSet = CountedSet<AArrow>()
 aCountedSet[.iron] // 0
 var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
 myCountedSet[.iron] // 4
@@ -11,13 +11,28 @@ myCountedSet.remove(.dwarvish) // 0
 myCountedSet.remove(.magic) // 0
 */
 
-struct CountedSet<H: Hashable> {
-	var Arr: [H]
+struct CountedSet<E: Hashable> {
+	private (set) var elements: [E]?
+	
+	subscript(_ element: E) -> Int {
+		guard let elements = elements else { return 0 }
+		
+		var i = 0
+		for e in elements {
+			if e == element {
+				break
+			} else {
+				i += 1
+			}
+		}
+		return i
+	}
 }
 
-enum Arrow {
-	case iron, elven, dwarvish, magic, silver
-}
+enum Arrow { case iron, elven, dwarvish, magic, silver }
 
-var aCountedSet = CountedSet<Arrow>(Arr: [.elven])
+
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron]
+
 
