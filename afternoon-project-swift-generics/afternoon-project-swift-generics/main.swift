100cc2f75360a664499fed8de8b1e2313f827c1c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-11 12:37:50 -0700

    init

---
+++ afternoon-project-swift-generics/afternoon-project-swift-generics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  afternoon-project-swift-generics
+//
+//  Created by Hector Steven on 6/11/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
