8c6ebcaaab5a0fe41aad1e2f20588fd303153d1b
Jake <jlconnerly@gmail.com>
2019-10-01 15:42:15 -0500

    Remove method working

--- Swift-Generics-Afternoon-Project.playground/Contents.swift
+++ Swift-Generics-Afternoon-Project.playground/Contents.swift
@@ -1,3 +1,77 @@
 import UIKit
 
+struct CountedSet<T: Hashable>: ExpressibleByArrayLiteral {
+    
+    typealias ArrayLiteralElement = T
+    
+    private(set) var elements: [T]
+    
+    init(arrayLiteral elements: T...) {
+        var holderArray:[T] = []
+        for element in elements {
+            holderArray.append(element)
+        }
+        self.elements = holderArray
+    }
+    
+    mutating func insert(element: T) {
+        elements.append(element)
+    }
+    
+    mutating func remove(element: T) -> [T]? {
+        
+        if elements.count > 0 {
+            var newArray: [T] = []
+            for elementRep in elements {
+                if elementRep != element {
+                    newArray.append(elementRep)
+                }
+                
+            }
+            return newArray
+        } else {
+            return nil
+        }
+    }
+    
+    func totalCount() -> Int {
+        var totalCount = 0
+        for _ in elements {
+            totalCount += 1
+        }
+        return totalCount
+    }
+    
+    func elementCount(element: T) -> Int {
+        var elementCount = 0
+        for i in elements {
+            if i == element {
+                elementCount += 1
+            }
+        }
+        return elementCount
+    }
+    
+    subscript(_ index: Int) -> T {
+        get {
+            return elements[index]
+            
+        }
+    }
+}
+
+enum Arrow {
+    case iron
+    case wooden
+    case elven
+    case dwarvish
+    case magic
+    case silver
+}
+
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+
+myCountedSet.elementCount(element: .iron)
+myCountedSet.remove(element: .iron)
+
 
