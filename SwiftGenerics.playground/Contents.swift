0d68328e3148dde31473292434a18477a3683aba
Robert Keffury <bobby.keffury@me.com>
2019-11-02 13:20:13 -0700

    Added some functionality.

--- SwiftGenerics.playground/Contents.swift
+++ SwiftGenerics.playground/Contents.swift
@@ -1,5 +1,33 @@
 import UIKit
 
-struct CountedSet: Hashable {
+struct CountedSet<Value>: Hashable {
+    
+    private(set) var 
+    
+    func insert() {
+        
+    }
+    
+    func remove() {
+        
+    }
     
 }
+
+
+enum Arrow {
+    case iron
+    case wooden
+    case elven
+    case dwarvish
+    case magic
+    case silver
+}
+
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
