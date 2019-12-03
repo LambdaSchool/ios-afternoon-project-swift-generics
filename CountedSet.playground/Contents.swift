fb4ec5d69dc40e7bad678709644dcd9eede28c72
Jon Bash <jonbash@protonmail.com>
2019-12-03 06:49:45 -0800

    add tests

--- CountedSet.playground/Contents.swift
+++ CountedSet.playground/Contents.swift
@@ -46,3 +46,11 @@ extension CountedSet: ExpressibleByArrayLiteral {
     typealias ArrayLiteralElement = Element
 }
 
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
