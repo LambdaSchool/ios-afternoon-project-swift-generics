93d9eeffbfa28bef72f4496a4186b37de18e7e0a
Jon Bash <jonbash@protonmail.com>
2019-12-03 06:27:29 -0800

    add contains method

--- CountedSet.playground/Contents.swift
+++ CountedSet.playground/Contents.swift
@@ -4,12 +4,15 @@ struct CountedSet<Element: Hashable> {
     private var values = [Element: UInt]()
     
     mutating func insert(_ newElement: Element) {
-        if values[newElement] != nil {
+        if contains(newElement) {
             values[newElement]! += 1
         } else {
             values[newElement] = 1
         }
     }
     
+    func contains(_ element: Element) -> Bool {
+        return values[element] != nil
+    }
 }
 
