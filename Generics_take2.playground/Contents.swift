7ae035861b63b6bfc0f8a8f25991d6ba7069d7cb
Craig Swanson <cswanson02@me.com>
2020-01-19 15:36:39 -1000

    Implemented count and insert methods in Struct

--- Generics_take2.playground/Contents.swift
+++ Generics_take2.playground/Contents.swift
@@ -3,17 +3,35 @@ import Foundation
 
 
 struct CountedSet<Element: Hashable> {
+    
     typealias DictionaryType = [Element : Int]
-
-    // Underlying, private storage, that is the same type of dictionary
-    // that we previously was using at the call site
-    private var myCountedSet = DictionaryType()
-
-    // Enable our collection to be initialized with a dictionary
-    init(myCountedSet: DictionaryType) {
-        self.myCountedSet = myCountedSet
+    private var privateDictionary = DictionaryType()
+    
+    func currentCount(_ element: Element) -> Int {
+        return privateDictionary[element] ?? 0
+    }
+    
+    mutating func insert(_ element: Element) {
+        if currentCount(element) == 0 {
+            privateDictionary[element] = 1
+        } else {
+            let newCount = currentCount(element) + 1
+            privateDictionary[element] = newCount
+        }
     }
 }
+
+var testRun = CountedSet<Int>()
+testRun.insert(44)
+print(testRun)
+testRun.insert(44)
+print(testRun)
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var newTest = CountedSet<Arrow>()
+testRun[44]
+
+
+
 
 extension CountedSet: Collection {
     // Required nested types, that tell Swift what our collection contains
@@ -21,27 +39,27 @@ extension CountedSet: Collection {
     typealias Element = DictionaryType.Element
 
     // The upper and lower bounds of the collection, used in iterations
-    var startIndex: Index { return myCountedSet.startIndex }
-    var endIndex: Index { return myCountedSet.endIndex }
+    var startIndex: Index { return privateDictionary.startIndex }
+    var endIndex: Index { return privateDictionary.endIndex }
 
     // Required subscript, based on a dictionary index
     subscript(index: Index) -> Iterator.Element {
-        get { return myCountedSet[index] }
+        get { return privateDictionary[index] }
     }
 
     // Method that returns the next index when iterating
     func index(after i: Index) -> Index {
-        return myCountedSet.index(after: i)
+        return privateDictionary.index(after: i)
     }
 }
 
 extension CountedSet: ExpressibleByDictionaryLiteral {
-    typealias Key = Element
-    typealias Value = Int
+//    typealias Key = Element
+//    typealias Value = Int
 
     init(dictionaryLiteral elements: (Element, Int)...) {
         for (element, count) in elements {
-            myCountedSet[element] = count
+            privateDictionary[element] = count
         }
     }
 }
