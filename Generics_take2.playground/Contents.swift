4d77667527170c6b0f9afc7d037451a1e0638d7c
Craig Swanson <cswanson02@me.com>
2020-01-19 16:54:14 -1000

    Final MVP

--- Generics_take2.playground/Contents.swift
+++ Generics_take2.playground/Contents.swift
@@ -11,55 +11,60 @@ struct CountedSet<Element: Hashable> {
         return privateDictionary[element] ?? 0
     }
     
-    mutating func insert(_ element: Element) {
+    subscript(index: Element) -> Int {
+        get {
+            return privateDictionary[index]!
+        }
+        set (count) {
+            privateDictionary[index]! = count
+        }
+    }
+    
+    mutating func insert(_ element: Element) -> Int {
         if currentCount(element) == 0 {
             privateDictionary[element] = 1
         } else {
             let newCount = currentCount(element) + 1
             privateDictionary[element] = newCount
         }
+        return currentCount(element)
+    }
+    
+    mutating func remove(_ element: Element) -> Int {
+        switch currentCount(element) {
+        case 0:
+            return 0
+        case 1:
+            privateDictionary.removeValue(forKey: element)
+            return 0
+        default:
+            let newCount = currentCount(element) - 1
+            privateDictionary[element] = newCount
+            return newCount
+        }
     }
 }
+
+extension CountedSet: ExpressibleByArrayLiteral {
+    typealias ArrayLiteralElement = Element
+    
+    init(arrayLiteral: Element...) {
+        for element in arrayLiteral {
+            self.insert(element)
+        }
+    }
+}
+
 
 var testRun = CountedSet<Int>()
 testRun.insert(44)
 print(testRun)
 testRun.insert(44)
 print(testRun)
+testRun.remove(44)
+testRun[44] = 4
+testRun.remove(44)
+
 enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
 var newTest = CountedSet<Arrow>()
-testRun[44]
 
-
-
-
-extension CountedSet: Collection {
-    // Required nested types, that tell Swift what our collection contains
-    typealias Index = DictionaryType.Index
-    typealias Element = DictionaryType.Element
-
-    // The upper and lower bounds of the collection, used in iterations
-    var startIndex: Index { return privateDictionary.startIndex }
-    var endIndex: Index { return privateDictionary.endIndex }
-
-    // Required subscript, based on a dictionary index
-    subscript(index: Index) -> Iterator.Element {
-        get { return privateDictionary[index] }
-    }
-
-    // Method that returns the next index when iterating
-    func index(after i: Index) -> Index {
-        return privateDictionary.index(after: i)
-    }
-}
-
-extension CountedSet: ExpressibleByDictionaryLiteral {
-//    typealias Key = Element
-//    typealias Value = Int
-
-    init(dictionaryLiteral elements: (Element, Int)...) {
-        for (element, count) in elements {
-            privateDictionary[element] = count
-        }
-    }
-}
