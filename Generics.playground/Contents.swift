857a86d9de926d6c0e6c4b852a39a9a75da9e3e0
Enrique Gongora <EGTech24@gmail.com>
2020-03-10 12:57:55 -0700

    Step 2 Complete

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -20,3 +20,20 @@ extension CountedSet {
         }
     }
 }
+
+// Support subscripting to look up current values. Return 0 for any value that is not found.
+
+extension CountedSet {
+    subscript(_ member: Element) -> Int {
+        return storage[member] ?? 0
+    }
+}
+
+//enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+//var aCountedSet = CountedSet<Arrow>()
+//aCountedSet[.iron] // 0
+//var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+//myCountedSet[.iron] // 4
+//myCountedSet.remove(.iron) // 3
+//myCountedSet.remove(.dwarvish) // 0
+//myCountedSet.remove(.magic) // 0
