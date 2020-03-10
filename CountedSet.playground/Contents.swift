f89259dc7014d044a39fb3c5768a58ce839eb456
scottharris86 <scottharrisdev@gmail.com>
2020-03-10 18:05:21 -0400

    cleaner 1 liners

--- CountedSet.playground/Contents.swift
+++ CountedSet.playground/Contents.swift
@@ -4,56 +4,31 @@ struct CountedSet<T: Hashable>: ExpressibleByArrayLiteral {
     
     init(arrayLiteral: T...) {
         for element in arrayLiteral {
-            var count = 0
-            if let theElement = elements[element] {
-                count = theElement
-            }
-            count += 1
-            elements[element] = count
+            let count = (elements[element] ?? 0) + 1
+            elements.updateValue(count, forKey: element)
         }
     }
     
     
     private(set) var elements: [T: Int] = [T: Int]()
     
-    mutating func insert(element: T) -> Int {
-        if let myElement = elements[element] {
-            let count = myElement + 1
-            elements.updateValue(count, forKey: element)
-            return count
-        } else {
-            elements[element] = 1
-            return 1
-        }
-        
+    mutating func insert(element: T) {
+        let count = (elements[element] ?? 0) + 1
+        elements[element] = count
     }
     
-    mutating func remove(_ element: T) -> Int {
-        if let myElement = elements[element] {
-            if myElement == 1 {
-                elements.removeValue(forKey: element)
-                return 0
-            } else {
-                let count = myElement - 1
-                elements.updateValue(count, forKey: element)
-                return count
-            }
-            
+    mutating func remove(_ element: T) {
+        let count = (elements[element] ?? 0) - 1
+        if count < 0 {
+            elements.removeValue(forKey: element)
+        } else {
+            elements[element] = count
         }
-        return 0
     }
     
     subscript(_ member: T) -> Int {
         get {
             return elements[member] ?? 0
-        }
-        set {
-            var count = 0
-            if let theElement = elements[member] {
-                count = theElement
-            }
-            count += 1
-            elements[member] = count
         }
     }
     
