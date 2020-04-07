93b0f95e76fd11bd11d8d24b734f92575a6443b3
Brad <braddiroff@icloud.com>
2020-04-07 15:43:16 -0400

    Just started

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+struct CountedSet<Element> where Element: Hashable {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+//myCountedSet[.iron] // 4
+//myCountedSet.remove(.iron) // 3
+//myCountedSet.remove(.dwarvish) // 0
+//myCountedSet.remove(.magic) // 0
