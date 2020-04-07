98a7aa821f6fb1f98f8c52e77fdc96fc3808f4b9
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-07 22:42:01 +0100

    Subripting and Count Related Properties Added

--- Generics Challenge.playground/Contents.swift
+++ Generics Challenge.playground/Contents.swift
@@ -3,6 +3,22 @@ import UIKit
 struct CountedSet<Element: Hashable> {
     private(set) var elements: [Element: Int] = [:]
     
+    // MARK: Count Related Properties
+    
+    var count: Int {
+        return elements.count
+    }
+    
+    var isEmpty: Bool {
+        if elements.isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // MARK: Insert and Remove Methods
+    
     mutating func insert(element: Element) {
         elements[element] = (elements[element] ?? 0) + 1
     }
@@ -10,5 +26,11 @@ struct CountedSet<Element: Hashable> {
     mutating func remove(element: Element) {
         guard !elements.isEmpty else { return }
         (elements[element] ?? 0) - 1
+    }
+    
+    // MARK: Subcripting
+    
+    subscript(_ member: Element) -> Int {
+        return elements[member] ?? 0
     }
 }
