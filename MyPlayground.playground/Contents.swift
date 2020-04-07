e0f99e4e359cb5531067b11a159946f8a891d51e
Hector L <hectoremil21@hotmail.com>
2020-04-07 17:30:36 -0400

    Working

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,2 +1,28 @@
 import Cocoa
 
+struct CountedSet<Element: Hashable> {
+    private var memCount: [Element : Int]
+    var count: Int {
+        return memCount.count
+    }
+    
+    mutating func insert(member: Element) {
+        if memCount[member] == nil {
+            memCount[member] = 1
+        } else {
+            memCount[member]! += 1
+        }
+    }
+    
+    mutating func remove(for member: Element) {
+        if memCount[member] == nil {
+            memCount[member] = 1
+        } else {
+            memCount[member]! -= 1
+        }
+    }
+    
+    subscript(_ member: Element) -> Int {
+        return memCount[member] ?? 0
+    }
+}
