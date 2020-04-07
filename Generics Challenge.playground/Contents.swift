47298c2d73f1846f15320ad2ca33e26756e65681
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-07 22:30:44 +0100

    Insert and Remove Methods

--- Generics Challenge.playground/Contents.swift
+++ Generics Challenge.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-
+struct CountedSet<Element: Hashable> {
+    private(set) var elements: [Element: Int] = [:]
+    
+    mutating func insert(element: Element) {
+        elements[element] = (elements[element] ?? 0) + 1
+    }
+    
+    mutating func remove(element: Element) {
+        guard !elements.isEmpty else { return }
+        (elements[element] ?? 0) - 1
+    }
+}
