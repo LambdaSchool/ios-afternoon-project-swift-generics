85f5bcbc5df4b78ba58aaf4973f34f352dbe2b7f
Brad <braddiroff@icloud.com>
2020-04-07 18:58:55 -0400

    Almost done

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -5,30 +5,49 @@ struct CountedSet<Element> : ExpressibleByArrayLiteral where Element: Hashable  
     private(set) var dictionary: [Int: Element] = [:]
     
         init(arrayLiteral elements: Element...) {
-            
-            print("SOMETHING HAPPENS")
-            print(elements.count)
+            print("ok it's initialized")
             for element in elements {
+                insert(element: element)
                 print(element)
-            //    dictionary[element.hashValue : element]
+                print(elements.count)
             }
+    }
+    
+    mutating func insert(element: Element) {
+        dictionary[dictionary.count] = element
+        print(dictionary.count)
+    }
+    
+    func remove(element: Element) {
+        if dictionary.values.contains(element) {
             
+        }
+    }
+    
+    subscript(_ element: Element) -> Int {
+    //    print("DICTIONARY ELEMENTS: \(dictionary)")
+        if dictionary.values.contains(element) {
+            print("ok so theres an element")
+            var count = 0
+            for (_, value) in dictionary {
+                if value == element {
+                    count += 1
+                }
+            }
+            return count } else {
+            print("theres no element")
+            return 0
+        }
+        
     }
 }
 
-
-
-
-
-
-
-
-
 enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
 var aCountedSet = CountedSet<Arrow>()
-//aCountedSet[.iron] // 0
-//var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
-//myCountedSet[.iron] // 4
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
 //myCountedSet.remove(.iron) // 3
 //myCountedSet.remove(.dwarvish) // 0
 //myCountedSet.remove(.magic) // 0
+
