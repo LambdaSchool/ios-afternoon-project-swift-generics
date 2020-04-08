f58d8130d06da33d977dd2ea28849df1c715e92b
Mark Gerrior <github@gerrior.com>
2020-04-08 12:23:02 -0700

    Implemented Insert

--- Generics Challenge.playground/Contents.swift
+++ Generics Challenge.playground/Contents.swift
@@ -24,10 +24,30 @@ struct CountedSet<Element: Hashable> {
         return dict.count > 0 ? false : true
     }
     
-    func insert(_ key: Element) {
+    /// Inserts an element into the set. If element already exists, element's count is increased by 1
+    /// - Parameter key: key of element to be inserted into set
+    /// - Returns: Number of element items remaing in set
+    mutating func insert(_ key: Element) -> Int {
+        if !contains(key) {
+            dict[key] = 0
+        }
         
+        #if WHY_DOESNT_THIS_WORK
+            guard var item = dict[key] else { return 0 }
+            
+            item += 1
+            
+            return item // dict[key] doesn't change. Just local item which is then thrown away.
+        #else
+            dict[key]! += 1
+            
+            return dict[key]!
+        #endif
     }
-
+    
+    /// Reduces element count by 1. Element is removed when count gets to 0
+    /// - Parameter key: key of element to be insereted into set
+    /// - Returns: Number of element items remaing in set
     mutating func remove(_ key: Element) -> Int {
         guard var item = dict[key] else { return 0 }
         
@@ -72,3 +92,8 @@ assert(myCountedSet.remove(.magic) == 0, "Should have been the last .magic")
 assert(myCountedSet.contains(.magic) == false, ".magic shouldn't be in the set")
 /// Reducing magic to zero means magic is removed and count is reduced by 1
 assert(myCountedSet.count == 2, "count should be 2")
+
+assert(myCountedSet.contains(.dwarvish) == false, ".dwarvish shouldn't be in the set yet")
+assert(myCountedSet.insert(.dwarvish)   == 1,     "This should be the first .dwarvish")
+assert(myCountedSet.contains(.dwarvish) == true,  ".dwarvish should exist")
+assert(myCountedSet.insert(.dwarvish)   == 2,     "This should be the second .dwarvish")
