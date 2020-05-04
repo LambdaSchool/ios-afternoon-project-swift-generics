eeeb9c9619bbd0ffeab4fcfe517432a224c3beb6
Harmony Radley <harmonyradley99@gmail.com>
2020-05-04 19:12:49 -0400

    Finished assignment

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 struct CountedSet<Element: Hashable> {
+    
+    // MARK: - Properties
+    
     private(set) var dictionary: [Element: Int] = [:]
     
     var count: Int? {
@@ -11,27 +14,40 @@ struct CountedSet<Element: Hashable> {
         }
     }
     
+    // MARK: - Methods
+    
     // Insert a new element into dictionary
-    mutating func insert(_ element: Element) {
-        
+    mutating func insert(element: Element) {
+        dictionary[element] = (dictionary[element] ?? 0) + 1
     }
     
-    // Remove an element from dictionary 
-    mutating func remove(_ element: Element) {
-        
+    // Remove an element from dictionary
+    mutating func remove(element: Element) {
+        dictionary[element] = nil
     }
     
+    // Calls CountedSet elements so you are able to access whichever element within an array using subscript syntax
     subscript(_ member: Element) -> Int {
-        if let result = dictionary[member] {
-            return result
-        } else {
-            return 0
-        }
+        return dictionary[member] ?? 0 
         
     }
 }
 
+// MARK: - Creating Property by calling CountedSet
+var countedSet = CountedSet(dictionary: ["Harmony" : 1, "Carl" : 2, "Boo" : 3])
+countedSet.dictionary
 
+// MARK: - Calling Methods
+
+countedSet.remove(element: "Boo")
+countedSet.insert(element: "Shelley")
+countedSet.count
+
+// MARK: - Subscript Calls
+countedSet["Harmony"]
+countedSet["Shelley"]
+
+// MARK: - Extension
 extension CountedSet: ExpressibleByArrayLiteral {
     init(arrayLiteral elements: Element...) {
         self.init()
@@ -40,3 +56,15 @@ extension CountedSet: ExpressibleByArrayLiteral {
         }
     }
 }
+
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.dictionary
+myCountedSet.remove(element: .iron) // 3
+myCountedSet.remove(element: .dwarvish) // 0
+myCountedSet.remove(element: .magic) // 0
+
