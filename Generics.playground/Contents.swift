fa95f32b17e45a595bf678569f36daa0e0c99f1b
Harmony Radley <harmonyradley99@gmail.com>
2020-05-04 18:30:57 -0400

    Created subscript method and an insert and remove function

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -3,15 +3,32 @@ import UIKit
 struct CountedSet<Element: Hashable> {
     private(set) var dictionary: [Element: Int] = [:]
     
+    var count: Int? {
+        if dictionary.isEmpty {
+            return 0
+        } else {
+            return dictionary.count
+        }
+    }
+    
+    // Insert a new element into dictionary
     mutating func insert(_ element: Element) {
         
     }
     
+    // Remove an element from dictionary 
     mutating func remove(_ element: Element) {
         
     }
     
-    
+    subscript(_ member: Element) -> Int {
+        if let result = dictionary[member] {
+            return result
+        } else {
+            return 0
+        }
+        
+    }
 }
 
 
