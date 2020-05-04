84818fe9eb03291db8a0b7705e2fb78a35561f77
Harmony Radley <harmonyradley99@gmail.com>
2020-05-04 17:43:36 -0400

    Trying to watch youtube tutorials to helo understand what I am doing

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,17 +1,25 @@
 import UIKit
 
-struct CountedSet<Element: Hashable>: ExpressibleByArrayLiteral {
-    init(arrayLiteral elements: Element...) {
-        self.init()
+struct CountedSet<Element: Hashable> {
+    private(set) var dictionary: [Element: Int] = [:]
+    
+    mutating func insert(_ element: Element) {
         
     }
     
-    typealias ArrayLiteralElement = Element
-    
-    
-
+    mutating func remove(_ element: Element) {
+        
+    }
     
     
 }
 
 
+extension CountedSet: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self.init()
+        for element in elements {
+            self.dictionary[element]
+        }
+    }
+}
