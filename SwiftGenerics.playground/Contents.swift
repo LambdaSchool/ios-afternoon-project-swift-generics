21dc9e884455aff23b5e11fdafe377a18a6ea4b0
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-04 23:38:28 -0700

    Working on the project

--- SwiftGenerics.playground/Contents.swift
+++ SwiftGenerics.playground/Contents.swift
@@ -11,15 +11,36 @@ struct CountedSet<Element: Hashable> {
             dict[element] = 1
         }
     }
+    
+    mutating func remove(_ element: Element) -> Int?  {
+        guard let count = dict[element] else { return 0 }
+        
+        if count > 1 {
+            dict[element] = count - 1
+        } else {
+            dict.removeValue(forKey: element)
+        }
+        
+        return count
+    }
+}
+
+extension CountedSet: ExpressibleByArrayLiteral {
+    init(arrayLiteral: Element...) {
+        self.init()
+        for element in arrayLiteral {
+            self.insert(element)
+        }
+    }
 }
 
 
 
-//enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
-//var aCountedSet = CountedSet<Arrow>()
-//aCountedSet[.iron] // 0
-//var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
-//myCountedSet[.iron] // 4
-//myCountedSet.remove(.iron) // 3
-//myCountedSet.remove(.dwarvish) // 0
-//myCountedSet.remove(.magic) // 0
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
