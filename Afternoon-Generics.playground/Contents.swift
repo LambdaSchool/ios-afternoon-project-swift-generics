88ae309a7ec3cef9497fc97de07f0223d250f6ce
Breena <breena_greek@live.com>
2020-05-06 23:17:58 -0400

    Insert working, working on remove

--- Afternoon-Generics.playground/Contents.swift
+++ Afternoon-Generics.playground/Contents.swift
@@ -11,34 +11,37 @@ struct CountedSet <T: Hashable>  {
         return count == 0
     }
     
+    //    mutating func insert(_ element: T) -> Int {
+    //        if let index = dictionary.index(forKey: element) {
+    //            var elementCount = dictionary[index].value
+    //            elementCount + 1
+    //            dictionary.updateValue(elementCount, forKey: element)
+    //            return elementCount
+    //        } else {
+    //            dictionary.updateValue(1, forKey: element)
+    //            return 1
+    //        }
+    //    }
+    
     mutating func insert(_ element: T) {
-        if let index = dictionary.index(forKey: element) {
-            if let elementCount = dictionary[element] {
-                elementCount + 1
-            } else {
-                dictionary[element] = 1
-            }
+        if var _ = dictionary[element] {
+            dictionary[element]! += 1
+        } else {
+            dictionary[element] = 1
         }
     }
     
     mutating func remove(_ element: T) -> Int {
-        if let index = dictionary.index(forKey: element) {
-            if let elementCount = dictionary[element] {
-                elementCount - 1
-            } else {
-                dictionary[element] = 1
-            }
+        if var _ = dictionary[element] {
+            dictionary[element]! -= 1
+        } else {
+            dictionary[element] = 1
         }
         return dictionary[element] ?? 0
     }
     
     subscript(_ element: T) -> Int {
-        if let index = dictionary.index(forKey: element) {
-            return dictionary[index].value
-        } else {
-            return 0
-            
-        }
+        return dictionary[element] ?? 0
     }
 }
 
@@ -51,3 +54,12 @@ extension CountedSet: ExpressibleByArrayLiteral {
         }
     }
 }
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
