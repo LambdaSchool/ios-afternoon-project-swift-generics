e5d3336bee82892f97d6e815726b401f24c9369c
Matt Martindale <martindalematt18@yahoo.com>
2020-05-06 18:20:57 -0600

    Created struct and implemented insert and remove methods

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+struct CountedSet<Element: Hashable> {
+    var set: Set<Element> = []
+    
+    private var dictionary: [Int:Element] = [:]
+    
+    mutating func insert(obj: Element) {
+        set.insert(obj)
+    }
+    
+    mutating func remove(obj: Element) {
+        set.remove(obj)
+    }
+    
+}
+
+var mySet = CountedSet<String>()
+mySet.insert(obj: "Hey Hey")
+print(mySet.set)
+mySet.remove(obj: "Hey ey")
+print(mySet.set)
