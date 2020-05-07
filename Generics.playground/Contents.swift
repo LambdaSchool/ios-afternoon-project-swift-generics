fd97a3400ffe623bafe93d9ce375ba6a6287c423
hai666l <0xconr@protonmail.com>
2020-05-06 21:08:34 -0500

    subscript()

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,1 +1,23 @@
 import Foundation
+
+struct CountedSet<T:Hashable> {
+    private var innerDict = [T:Int]()
+    
+    func insert(_ ele: T) {
+        
+    }
+    
+    func remove(_ ele: T) {
+        
+    }
+    
+    subscript(_ member: T) -> Int {
+        guard innerDict[member] != nil else { return 0 }
+        return innerDict[member]!
+    }
+}
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+print(aCountedSet[.iron])
+//var myCountedSet: CountedSet<Arrow> = [.iron, .wooden, .magic, .magic]
