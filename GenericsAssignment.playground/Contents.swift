37dc5c126a62ab6c64e53f656030b34f3e1abea7
Harmony Radley <harmonyradley99@gmail.com>
2020-06-01 16:17:17 -0400

    Added insert and remove functions

--- GenericsAssignment.playground/Contents.swift
+++ GenericsAssignment.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 struct CountedSet<Element: Hashable> {
-    private(set) var dictionary = [Element: Int] = [:]
+    private(set) var dictionary: [Element: Int] = [:]
+
+
+    // MARK: - Methods
+
+    // Insert a new element into a dictionary
+    mutating func insert(element: Element) {
+        dictionary[element] = (dictionary[element] ?? 0) + 1
+    }
+
+    // Remove an element from dictionary
+    mutating func remove(element: Element) {
+        dictionary[element] = nil
+    }
 }
