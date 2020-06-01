55ed806c3319571fc318770ca63acda23e923f2c
Harmony Radley <harmonyradley99@gmail.com>
2020-06-01 16:23:03 -0400

    Added a count variable

--- GenericsAssignment.playground/Contents.swift
+++ GenericsAssignment.playground/Contents.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 struct CountedSet<Element: Hashable> {
+
+    // MARK: - Properties
+
     private(set) var dictionary: [Element: Int] = [:]
 
-
+    var count: Int? {
+        if dictionary.isEmpty {
+            return 0
+        } else {
+            return dictionary.count
+        }
+    }
+    
     // MARK: - Methods
 
     // Insert a new element into a dictionary
@@ -14,5 +24,12 @@ struct CountedSet<Element: Hashable> {
     // Remove an element from dictionary
     mutating func remove(element: Element) {
         dictionary[element] = nil
+    }
+
+    // Calls CountedSet elements so you are able to access whichever element within an array using subscript syntax
+    // grabbing a value from the dictionary and returning it.
+    subscript(_ member: Element) -> Int {
+        return dictionary[member] ?? 0
+
     }
 }
