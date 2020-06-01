ca57cd5d75193671be7f1a7838399fcdbb489cd2
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-06-01 14:25:28 -0700

    MVP?

--- Tableview-with-Entry.playground/Contents.swift
+++ Tableview-with-Entry.playground/Contents.swift
@@ -1,4 +1,4 @@
-import UIKit
+import Foundation
 
 struct CountedSet <T: Hashable> {
     private(set) var dictionary: [T: Int] = [:]
@@ -28,4 +28,26 @@ struct CountedSet <T: Hashable> {
         }
         return dictionary[element] ?? 0
     }
+    subscript(_ element: T) -> Int {
+        return dictionary[element] ?? 0
+    }
 }
+
+extension CountedSet: ExpressibleByArrayLiteral {
+    
+    init(arrayLiteral element: T...) {
+        self.init()
+        for element in element {
+            self.insert(element)
+        }
+    }
+}
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron]
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron]
+myCountedSet.remove(.iron)
+myCountedSet.remove(.dwarvish)
+myCountedSet.remove(.magic)
