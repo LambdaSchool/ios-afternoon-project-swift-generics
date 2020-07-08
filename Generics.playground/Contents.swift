1367a414e118aa76537b3abeba01e6321ba078fa
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-08 12:34:42 -0700

    Created dictonary property, added variables, conformed to array literal

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,6 +1,27 @@
 import Foundation
 
-struct CountedSet<Element>:Hashable{
-
-
+struct CountedSet<Element: Hashable>{
+    private var myDictionary = [Element : Int]()
+    
+    var count: Int{
+        return myDictionary.count
+    }
+    
+    var isEmpty: Bool{
+        return count == 0
+    }
+    
+    
 }
+
+extension CountedSet: ExpressibleByArrayLiteral {
+    init(arrayLiteral: Element...) {
+        self.init()
+        for element in arrayLiteral {
+            self.insert(element: element)
+        }
+    }
+}
+
+
+
