e7e306c44e5b21f410131d916014fcb5a6282257
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-08 12:36:40 -0700

    Stubbed out insert and remove functions. Added Subscript

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,17 +1,27 @@
 import Foundation
 
 struct CountedSet<Element: Hashable>{
+    //MARK: - Properties
     private var myDictionary = [Element : Int]()
-    
     var count: Int{
         return myDictionary.count
     }
-    
     var isEmpty: Bool{
         return count == 0
     }
     
+    //MARK: - Functions
+    mutating func insert(element: Element){
     
+    }
+    
+    mutating func remove(element: Element){
+        
+    }
+    
+    subscript(_ member: Element) -> Int{
+        return myDictionary[member] ?? 0
+    }
 }
 
 extension CountedSet: ExpressibleByArrayLiteral {
