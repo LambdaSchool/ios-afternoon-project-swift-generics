2936cdbc2cc136d32afdd07d128283a7202ab181
Bronson <bronsonmullens@icloud.com>
2020-07-08 16:11:49 -0700

    Set up struct to work with String : Int dictionaries

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+import Foundation
+
+struct CountedSet: Hashable {
+    
+    var set: [String : Int] = [:]
+    
+    mutating func insert(entry: String, count: Int) {
+        set[entry] = count
+    }
+    
+    mutating func remove(entry: String) {
+        set.removeValue(forKey: entry)
+    }
+    
+    mutating func countEntries() -> Int {
+        var count = 0
+        
+        guard !set.isEmpty else { return 0 }
+        
+        for _ in set {
+            count += 1
+        }
+        
+        return count
+    }
+    
+}
+
+var myCountedSet = CountedSet()
+
+myCountedSet.insert(entry: "Hello", count: 3)
+myCountedSet.insert(entry: "World", count: 9)
+
+print(myCountedSet.set)
+print(myCountedSet.countEntries())
+
+myCountedSet.remove(entry: "World")
+
+print(myCountedSet.set)
+print(myCountedSet.countEntries())
+
+myCountedSet.remove(entry: "Hello")
+
+print(myCountedSet.countEntries())
