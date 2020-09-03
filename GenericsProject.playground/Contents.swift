91e58ec3e22bf44d3d079220460c728a8522d8fa
Ben James <bejjmail@gmail.com>
2020-09-02 20:23:02 -0700

    created struct

--- GenericsProject.playground/Contents.swift
+++ GenericsProject.playground/Contents.swift
@@ -1,2 +1,20 @@
 import UIKit
 
+struct CountedSet<Element: Hashable> {
+private(set) var elements: [Element: Int] = [:]
+
+subscript(_ key: Element) -> Int {
+    set {
+        elements[key] = newValue
+    }
+    get {
+        if let count = elements[key] {
+            return count
+        }
+        return 0
+    }
+}
+    
+    
+    
+}
