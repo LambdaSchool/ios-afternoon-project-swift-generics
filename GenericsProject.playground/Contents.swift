5445e0f664a01b75c1cbed24ee77de72950fcd0b
Ben James <bejjmail@gmail.com>
2020-09-02 22:36:56 -0700

    did not work, reworked struct and subscript and completed project

--- GenericsProject.playground/Contents.swift
+++ GenericsProject.playground/Contents.swift
@@ -1,20 +1,64 @@
 import UIKit
 
 struct CountedSet<Element: Hashable> {
-private(set) var elements: [Element: Int] = [:]
 
-subscript(_ key: Element) -> Int {
-    set {
-        elements[key] = newValue
+    private var elements = [Element: Int]()
+
+    var count: Int {
+        return elements.count
     }
-    get {
-        if let count = elements[key] {
-            return count
+
+    var isEmpty: Bool {
+        return count == 0
+    }
+
+    mutating func insert(_ element: Element) {
+        if let elementCount = elements[element] {
+            elements[element] = elementCount + 1
+        } else {
+            elements[element] = 1
         }
-        return 0
+    }
+
+    mutating func remove(_ element: Element) {
+        guard let elementCount = elements[element] else { return }
+
+        if elementCount > 1 {
+            elements[element] = elementCount - 1
+        } else {
+            elements[element] = nil
+        }
+    }
+
+    subscript(_ member: Element) -> Int {
+        return elements[member] ?? 0
     }
 }
-    
-    
-    
+
+extension CountedSet: ExpressibleByArrayLiteral {
+    init(arrayLiteral: Element...) {
+        for element in arrayLiteral {
+            self.insert(element)
+        }
+    }
 }
+
+extension CountedSet {
+    func contains(_ element: Element) -> Bool {
+        return elements.keys.contains(element)
+    }
+}
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron]      // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron]     // 4
+myCountedSet.remove(.iron)
+myCountedSet[.iron]     // 3
+myCountedSet.remove(.dwarvish)
+myCountedSet[.dwarvish] // 0
+myCountedSet.remove(.magic)
+myCountedSet[.magic]    // 0
+
